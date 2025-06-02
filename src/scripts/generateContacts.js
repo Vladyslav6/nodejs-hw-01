5dd778207e20bd3c8fb397f4ff1980c18c168e11
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-06-02 17:43:52 +0300

    addFaker

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,12 +1,12 @@
-// import { createFakeContact } from "../utils/createFakeContact.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
 import { writeContacts } from "../utils/writeContacts.js";
 
 const generateContacts = async (number) => {
-//   const fakeNumber = createFakeContact()
-//   console.log(fakeNumber)
-    writeContacts('num222ber2')
-    console.log("d")
-    console.log(number)
+  const fakeNumber = createFakeContact();
+  console.log(fakeNumber);
+    writeContacts('num222ber2');
+    console.log("d");
+    console.log(number);
 };
 
 generateContacts(5);

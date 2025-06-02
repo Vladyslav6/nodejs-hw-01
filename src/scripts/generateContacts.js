0d32c5f15255f1af98fc41ff629aad24ed94aeba
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-06-02 16:59:58 +0300

    addGenerate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,9 @@
-const generateContacts = async (number) => {};
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+    writeContacts('num222ber2')
+    console.log("d")
+    console.log(number)
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -4,9 +4,10 @@ import fs from 'node:fs/promises';
 export const writeContacts = async (updatedContacts) => {
   {
     const data = updatedContacts;
+    console.log("writeContact ",data)
     try {
       await fs.writeFile(PATH_DB, data, 'utf8');
-      console.log('Дані успішно записані у файл.');
+      console.log('Дані успішно записані у файл.',PATH_DB);
     } catch (err) {
       console.error('Помилка запису у файл:', err);
     }

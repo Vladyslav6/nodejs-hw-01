54163e83e37e2d8a1b965e0edba8392ca21cd796
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2025-06-03 17:54:01 +0300

    addLastStep

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,16 @@
-export const removeAllContacts = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+
+
+export const removeAllContacts = async () => {
+        const contacts = ([]);
+    try {
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf8');
+      console.log('Дані успішно записані у файл.', PATH_DB);
+    } catch (err) {
+      console.error('Помилка запису у файл:', err);
+    }
+};
 
 removeAllContacts();
